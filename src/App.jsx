097ccca8281e1a0f7446514fcd320bb95b014714
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-05-21 13:15:02 -0400

    Added MainLayout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <Routes>
-      <Route path="/auth/register" element={<><h1>Register</h1><Button variant="bocesPrimary">Register</Button><Button variant="outline">another button</Button></>} />
-      <Route path="/auth/login" element={<h1>Login</h1>} />
+      <Route path="/" element={<MainLayout />}>
+        <Route
+          path="/auth/register"
+          element={
+            <>
+              <h1>Register</h1>
+              <Button variant="bocesPrimary">Register</Button>
+              <Button variant="outline">another button</Button>
+            </>
+          }
+        />
+        <Route path="/auth/login" element={<h1>Login</h1>} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,47 @@
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header>
+        <nav className="mb-5 p-5 shadow">
+          <div className="max-w-36">
+            <img
+              src="https://www.nassauboces.org/cms/lib/NY01928409/Centricity/Template/GlobalAssets/images///logos/NBlogo-website3.png"
+              alt="Nassau BOCES logo"
+            />
+          </div>
+        </nav>
+      </header>
+
+      <main className="grow">
+        <Outlet />
+      </main>
+
+      <footer className="bg-bocesPrimary p-5 text-white">
+        <p>
+          &copy; 2002 Nassau BOCES. All Rights Reserved.{" "}
+          <a
+            className="underline"
+            href="https://nbws.nasboces.org/artsined/main/company/terms.asp"
+            target="_blank"
+          >
+            Terms & Conditions
+          </a>
+          .{" "}
+          <a
+            className="underline"
+            href="https://nbws.nasboces.org/artsined/main/company/privacy.asp"
+            target="_blank"
+          >
+            Privacy Policy
+          </a>
+          .
+        </p>
+        <p>Produced by SynergyMedia, Inc.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default MainLayout;
