fd1fd56057916516686c4504c622ec3f42fcf07e
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-05-22 12:18:58 -0400

    Added PasswordInput custom component.

---
+++ src/components/ui/password-input.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+import { EyeNoneIcon, EyeOpenIcon } from "@radix-ui/react-icons";
+
+const PasswordInput = React.forwardRef(({ className, ...props }, ref) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <>
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 pr-8 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        <div className="absolute right-3 top-3">
+          {showPassword ? (
+            <EyeOpenIcon
+              className="select-none hover:cursor-pointer"
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <EyeNoneIcon
+              className="select-none hover:cursor-pointer"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+});
+PasswordInput.displayName = "PasswordInput";
+
+export { PasswordInput };
