67a8bf594e022cf3340c1606eb16ef1f73f29ac9
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-08-30 15:25:08 -0400

    Added the api call to the fiscal years and added error checking.

--- src/pages/ProgramsPage.jsx
+++ src/pages/ProgramsPage.jsx
@@ -10,6 +10,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { buttonVariants } from "@/components/ui/button";
+import { useQueryForDataQuery } from "@/redux/api/quickbaseApi";
 
 const BUTTON_LINKS = [
   { label: "New Program", url: "/create-program", isTargetBlank: false },
@@ -19,6 +20,28 @@ const BUTTON_LINKS = [
 ];
 
 const ProgramsPage = () => {
+  const {
+    data: fiscalYearsData,
+    isError: isFiscalYearsDataError,
+    error: fiscalYearsDataError,
+  } = useQueryForDataQuery({
+    from: import.meta.env.VITE_QUICKBASE_FISCAL_YEARS_TABLE_ID,
+    select: [3, 6],
+  });
+
+  if (isFiscalYearsDataError) {
+    console.error(fiscalYearsDataError);
+    return (
+      <>
+        <p>There was an error getting the fiscal years data.</p>
+        <p>
+          Status: {fiscalYearsDataError.status} -{" "}
+          {fiscalYearsDataError.data.message}
+        </p>
+      </>
+    );
+  }
+
   return (
     <>
       <div>
@@ -29,9 +52,12 @@ const ProgramsPage = () => {
           <SelectContent>
             <SelectGroup>
               <SelectLabel>Fiscal years</SelectLabel>
-              <SelectItem value="apple">2023-24</SelectItem>
-              <SelectItem value="banana">2024-25</SelectItem>
-              <SelectItem value="blueberry">2025-26</SelectItem>
+              {fiscalYearsData &&
+                fiscalYearsData.data.map((item) => (
+                  <SelectItem key={item[3].value} value={item[6].value}>
+                    {item[6].value}
+                  </SelectItem>
+                ))}
             </SelectGroup>
           </SelectContent>
         </Select>
@@ -42,7 +68,7 @@ const ProgramsPage = () => {
           <Link
             key={index}
             to={link.url}
-            target={link.isTargetBlank ? "_blank" : ""}
+            target={link.isTargetBlank ? "_blank" : null}
             className={buttonVariants({ variant: "bocesSecondary" })}
           >
             {link.label}
