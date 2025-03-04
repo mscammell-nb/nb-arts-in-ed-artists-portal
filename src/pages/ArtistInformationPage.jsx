e1951b39965b3dbc1d7c41e18662e070eaf5312c
klobos42 <klobos42@nasboces.org>
2025-03-04 15:07:33 -0500

    Artist Information Page dsplays basic information about the artists/org.

--- src/pages/ArtistInformationPage.jsx
+++ src/pages/ArtistInformationPage.jsx
@@ -1,10 +1,68 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQueryForDataQuery } from "@/redux/api/quickbaseApi";
+
+// TODO: Reset Password Functionality
+
 const ArtistInformationPage = () => {
+  const artistRecordId = localStorage.getItem("artistRecordId");
+  const {
+    data: artistsData,
+    isLoading: isArtstsLoading,
+    isError: isPerformersError,
+    error: artistsError,
+  } = useQueryForDataQuery({
+    from: import.meta.env.VITE_QUICKBASE_ARTISTS_TABLE_ID,
+    select: [6, 7, 9, 19, 12, 11],
+    where: `{3.EX.${artistRecordId}}`,
+  });
+
   return (
-    <>
-      <h1 className="mb-10 scroll-m-20 text-center text-3xl font-bold tracking-tight lg:text-5xl">
-        TODO: Artist Information
-      </h1>
-    </>
+    <div className="flex w-full flex-col gap-3">
+      <div className="w-full rounded border border-gray-200 bg-white p-2.5">
+        <p className="text-xl font-semibold">Artist Information</p>
+      </div>
+      {isArtstsLoading ? (
+        <div className="flex w-full flex-col items-start gap-4 rounded border border-gray-200 bg-white p-2.5">
+          <Skeleton className="h-[20px] w-[300px] rounded-full" />
+          <Skeleton className="h-[20px] w-[200px] rounded-full" />
+          <Skeleton className="mt-2 h-[20px] w-[300px] rounded-full" />
+          <Skeleton className="h-[20px] w-[200px] rounded-full" />
+          <Skeleton className="mt-2 h-[20px] w-[300px] rounded-full" />
+          <Skeleton className="h-[20px] w-[200px] rounded-full" />
+        </div>
+      ) : (
+        <div className="flex w-full flex-col gap-3 rounded border border-gray-200 bg-white p-2.5">
+          <div>
+            <Label className="font-semibold">Artist / Org</Label>
+            <p>{artistsData?.data && artistsData.data[0][6].value}</p>
+          </div>
+          <div>
+            <Label className="font-semibold">Number of Performers</Label>
+            <p>NUMBER HERE</p>
+          </div>
+          <div>
+            <Label className="font-semibold">Email</Label>
+            <p>{artistsData?.data && artistsData.data[0][7].value}</p>
+          </div>
+
+          <div>
+            <Label className="font-semibold">Phone</Label>
+            <p>{artistsData?.data && artistsData.data[0][9].value}</p>
+          </div>
+
+          <div>
+            <Label className="font-semibold">Address</Label>
+            <p>{artistsData?.data && artistsData.data[0][12].value}</p>
+          </div>
+
+          <p className="mt-3 w-fit cursor-pointer text-sm text-blue-500 hover:underline">
+            Reset Password
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
 
