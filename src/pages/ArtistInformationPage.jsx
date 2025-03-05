950f0d0c4ae3eca98d83df81820f18128bde82f2
klobos42 <klobos42@nasboces.org>
2025-03-05 13:45:40 -0500

    Users can update artists address.

--- src/pages/ArtistInformationPage.jsx
+++ src/pages/ArtistInformationPage.jsx
@@ -11,8 +11,17 @@ import {
 } from "@/components/ui/select";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { toast } from "@/components/ui/use-toast";
-import { useAddOrUpdateRecordMutation, useQueryForDataQuery } from "@/redux/api/quickbaseApi";
+import {
+  useAddOrUpdateRecordMutation,
+  useQueryForDataQuery,
+} from "@/redux/api/quickbaseApi";
 import { Pencil1Icon } from "@radix-ui/react-icons";
 import { useEffect, useRef, useState } from "react";
 
@@ -34,34 +43,79 @@ const ArtistItem = ({
   setValue,
   editing,
   dropdown = false,
+  address = false,
 }) => {
+  let item = (
+    <Input
+      type="text"
+      value={value}
+      className="max-w-[400px]"
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+  if (dropdown) {
+    item = (
+      <Select onValueChange={(val) => setValue(val)}>
+        <SelectTrigger className="w-[400px]">
+          <SelectValue placeholder="Number of Performers" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Less than Five">Less than 5</SelectItem>
+          <SelectItem value="More than Five">More than 5</SelectItem>
+        </SelectContent>
+      </Select>
+    );
+  } else if (address) {
+    console.log(value);
+    item = (
+      <>
+        <Label>Street line 1</Label>
+        <Input
+          className="max-w-[400px]"
+          value={address.street1}
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, street1: e.target.value }))
+          }
+        />
+        <Label>Street line 2</Label>
+        <Input
+          className="max-w-[400px]"
+          value={address.street2}
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, street2: e.target.value }))
+          }
+        />
+        <Label>City</Label>
+        <Input
+          className="max-w-[400px]"
+          value={address.city}
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, city: e.target.value }))
+          }
+        />
+        <Label>State</Label>
+        <Input
+          className="max-w-[400px]"
+          value={address.state}
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, state: e.target.value }))
+          }
+        />
+        <Label>Postal Code</Label>
+        <Input
+          className="max-w-[400px]"
+          value={address.zipCode}
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, zipCode: e.target.value }))
+          }
+        />
+      </>
+    );
+  }
   return (
     <div className="flex flex-col gap-1">
       <Label className="font-semibold">{label}</Label>
-      {editing ? (
-        <>
-          {dropdown ? (
-            <Select onValueChange={(val) => setValue(val)}>
-              <SelectTrigger className="w-[400px]">
-                <SelectValue placeholder="Number of Performers" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="Less than Five">Less than 5</SelectItem>
-                <SelectItem value="More than Five">More than 5</SelectItem>
-              </SelectContent>
-            </Select>
-          ) : (
-            <Input
-              type="text"
-              value={value}
-              className="max-w-[400px]"
-              onChange={(e) => setValue(e.target.value)}
-            />
-          )}
-        </>
-      ) : (
-        <p>{value || '-'}</p>
-      )}
+      {editing ? <>{item}</> : <p>{value || "-"}</p>}
     </div>
   );
 };
@@ -74,15 +128,18 @@ const ArtistInformationPage = () => {
   const [phoneVal, setPhoneVal] = useState("");
   const [addressVal, setAddressVal] = useState("");
   const [websiteVal, setWebsiteVal] = useState("");
+  const [addressObject, setAddressObject] = useState({});
 
   const artistRecordId = localStorage.getItem("artistRecordId");
-  const [updateArtist ,
-  {
-    isLoading: isUpdateArtistLoading,
-    isSuccess: isUpdateArtistSucess,
-    isError: isUpdateArtistError,
-    error: updateArtistError
-  }] = useAddOrUpdateRecordMutation();
+  const [
+    updateArtist,
+    {
+      isLoading: isUpdateArtistLoading,
+      isSuccess: isUpdateArtistSucess,
+      isError: isUpdateArtistError,
+      error: updateArtistError,
+    },
+  ] = useAddOrUpdateRecordMutation();
   let {
     data: artistsData,
     isLoading: isArtstsLoading,
@@ -90,91 +147,110 @@ const ArtistInformationPage = () => {
     error: artistsError,
   } = useQueryForDataQuery({
     from: import.meta.env.VITE_QUICKBASE_ARTISTS_TABLE_ID,
-    select: [6, 7, 9, 19, 12, 11, 31],
+    select: [6, 7, 9, 19, 12, 13, 14, 15, 16, 17, 11, 31],
     where: `{3.EX.${artistRecordId}}`,
   });
   useEffect(() => {
     if (artistsData) {
-      setArtistVal(artistsData.data[0][6].value);
-      setPerformersVal(artistsData.data[0][19].value);
-      setEmailVal(artistsData.data[0][7].value);
-      setPhoneVal(artistsData.data[0][9].value);
-      setAddressVal(artistsData.data[0][12].value);
-      setWebsiteVal(artistsData.data[0][31].value);
+      resetInformation();
     }
   }, [artistsData]);
 
-  useEffect(()=>{
-    if(isUpdateArtistSucess){
+  useEffect(() => {
+    if (isUpdateArtistSucess) {
       toast({
         title: "Operation successful!",
         description: "Artists information updated",
         variant: "success",
-      })
+      });
     }
-    if(isUpdateArtistError){
+    if (isUpdateArtistError) {
       toast({
         title: "There's been an error",
         description: "There was an error updating the artists information.",
         variant: "destructive",
       });
-      console.log(updateArtistError)
+      console.log(updateArtistError);
     }
-
-  }, [updateArtistError, isUpdateArtistSucess, isUpdateArtistError])
+  }, [updateArtistError, isUpdateArtistSucess, isUpdateArtistError]);
 
   const onSave = () => {
-    console.log(artistVal)
+    console.log(addressObject);
     updateArtist({
       to: import.meta.env.VITE_QUICKBASE_ARTISTS_TABLE_ID,
       data: [
         {
           3: {
-            value: artistRecordId
+            value: artistRecordId,
           },
           6: {
-            value: artistVal
+            value: artistVal,
           },
           19: {
-            value: performersVal
+            value: performersVal,
           },
           7: {
-            value: emailVal
+            value: emailVal,
           },
           9: {
-            value: phoneVal
+            value: phoneVal,
           },
-          // 12: {
-          //   value: addressVal
-          // },
+          13: { value: addressObject.street1 },
+          14: { value: addressObject.street2 },
+          15: { value: addressObject.city },
+          16: { value: addressObject.state },
+          17: { value: addressObject.zipCode },
+          18: {
+            value: "United States",
+          },
           31: {
-            value: websiteVal
-          }
-        }
-      ]
-    })
-    setEditing(false)
+            value: websiteVal,
+          },
+        },
+      ],
+    });
+    setEditing(false);
   };
   const cancelEdit = () => {
     setEditing(false);
+    resetInformation();
+  };
+
+  const resetInformation = () => {
     setArtistVal(artistsData.data[0][6].value);
     setPerformersVal(artistsData.data[0][19].value);
     setEmailVal(artistsData.data[0][7].value);
     setPhoneVal(artistsData.data[0][9].value);
     setAddressVal(artistsData.data[0][12].value);
     setWebsiteVal(artistsData.data[0][31].value);
+    setAddressObject({
+      street1: artistsData.data[0][13].value,
+      street2: artistsData.data[0][14].value,
+      city: artistsData.data[0][15].value,
+      state: artistsData.data[0][16].value,
+      zipCode: artistsData.data[0][17].value,
+    });
   };
 
   return (
     <div className="flex w-full flex-col gap-3">
       <div className="flex w-full items-center justify-between rounded border border-gray-200 bg-white p-2.5">
         <p className="text-xl font-semibold">Artist Information</p>
-        <Pencil1Icon
-          className="size-5 cursor-pointer text-blue-400"
-          onClick={() => {
-            setEditing(!editing);
-          }}
-        />
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Pencil1Icon
+                className="size-5 cursor-pointer text-blue-400"
+                onClick={() => {
+                  setEditing(!editing);
+                }}
+              />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Edit</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
       {isArtstsLoading ? (
         <div className="flex w-full flex-col items-start gap-4 rounded border border-gray-200 bg-white p-2.5">
@@ -215,8 +291,9 @@ const ArtistInformationPage = () => {
           <ArtistItem
             label="Address"
             value={addressVal}
-            setValue={setAddressVal}
+            setValue={setAddressObject}
             editing={editing}
+            address={addressObject}
           />
           <ArtistItem
             label="Website"
@@ -268,7 +345,6 @@ const ArtistInformationPage = () => {
               >
                 Cancel
               </Button>
-              {/* TODO: Implement save logic */}
               <Button className="w-fit" onClick={() => onSave()}>
                 Save
               </Button>
