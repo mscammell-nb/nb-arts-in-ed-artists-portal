4df7205ca87be82669c38866127e1d903ed8ae02
George Nakashyan <gnakashyan@nasboces.org>
2025-03-27 14:20:55 -0400

    feat: show payment type and payee name only if check

--- src/pages/ArtistInformationPage.jsx
+++ src/pages/ArtistInformationPage.jsx
@@ -127,6 +127,8 @@ const ArtistInformationPage = () => {
   const [addressVal, setAddressVal] = useState("");
   const [websiteVal, setWebsiteVal] = useState("");
   const [addressObject, setAddressObject] = useState({});
+  const [paymentType, setPaymentType] = useState("");
+  const [payeeName, setPayeeName] = useState("");
 
   const artistRecordId = localStorage.getItem("artistRecordId");
   const [
@@ -146,7 +148,9 @@ const ArtistInformationPage = () => {
     error: artistsError,
   } = useQueryForDataQuery({
     from: import.meta.env.VITE_QUICKBASE_ARTIST_REGISTRATIONS_TABLE_ID,
-    select: [3, 6, 7, 8, 9, 11, 12, 14, 15, 16, 17, 18, 19, 21, 23, 24, 25],
+    select: [
+      3, 6, 7, 8, 9, 11, 12, 14, 15, 16, 17, 18, 19, 21, 23, 24, 25, 30, 31,
+    ],
     where: `{7.EX.${artistRecordId}.and.25.EX.${getCurrentFiscalYear()}}`,
   });
   useEffect(() => {
@@ -204,6 +208,12 @@ const ArtistInformationPage = () => {
           23: {
             value: websiteVal,
           },
+          30: {
+            value: paymentType,
+          },
+          31: {
+            value: payeeName,
+          },
         },
       ],
     });
@@ -228,11 +238,13 @@ const ArtistInformationPage = () => {
       state: artistsData.data[0][18].value,
       zipCode: artistsData.data[0][19].value,
     });
+    setPaymentType(artistsData.data[0][30].value);
+    setPayeeName(artistsData.data[0][31].value);
   };
 
   return (
     <div className="flex w-full max-w-[1200px] flex-col gap-3">
-      {(artistsData && !artistsData.data[0][6]) && (
+      {artistsData && !artistsData.data[0][6] && (
         <div className="flex w-full items-center justify-start gap-3 border border-yellow-500 bg-yellow-100 p-2 text-yellow-700">
           <CircleAlert size={20} />
           <p>
@@ -271,94 +283,114 @@ const ArtistInformationPage = () => {
           <Skeleton className="h-[20px] w-[200px] rounded-full" />
         </div>
       ) : (
-        <div className="flex w-full flex-col gap-3 rounded border border-gray-200 bg-white p-2.5">
-          <ArtistItem
-            label="Artist / Org"
-            value={artistVal}
-            setValue={setArtistVal}
-            editing={editing}
-          />
-          <ArtistItem
-            label="Number of Performers"
-            value={performersVal}
-            setValue={setPerformersVal}
-            editing={editing}
-            dropdown={true}
-          />
-          <ArtistItem
-            label="Email"
-            value={emailVal}
-            setValue={setEmailVal}
-            editing={editing}
-          />
-          <ArtistItem
-            label="Phone"
-            value={phoneVal}
-            setValue={setPhoneVal}
-            editing={editing}
-          />
-          <ArtistItem
-            label="Address"
-            value={addressVal}
-            setValue={setAddressObject}
-            editing={editing}
-            address={addressObject}
-          />
-          <ArtistItem
-            label="Website"
-            value={websiteVal}
-            setValue={setWebsiteVal}
-            editing={editing}
-          />
-          <Sheet>
-            <SheetTrigger className="mt-3 w-fit cursor-pointer text-sm text-blue-500 hover:underline">
-              Change Password
-            </SheetTrigger>
-            <SheetContent>
-              <div className="flex w-full flex-col gap-3">
-                <div>
-                  <p className="text-2xl font-bold">Change Password</p>
-                  <p className="text-gray-500">
-                    {" "}
-                    Ensure your account is using a long, random password to stay
-                    secure.
-                  </p>
-                </div>
-                <div>
-                  <Label className="font-semibold">Current Password</Label>
-                  <PasswordInput />
-                </div>
+        <div className="flex flex-row gap-4">
+          <div className="flex w-[50%] flex-col gap-3 rounded border border-gray-200 bg-white p-2.5">
+            <h1 className="text-xl font-semibold">General Information</h1>
+            <ArtistItem
+              label="Artist / Org"
+              value={artistVal}
+              setValue={setArtistVal}
+              editing={editing}
+            />
+            <ArtistItem
+              label="Number of Performers"
+              value={performersVal}
+              setValue={setPerformersVal}
+              editing={editing}
+              dropdown={true}
+            />
+            <ArtistItem
+              label="Email"
+              value={emailVal}
+              setValue={setEmailVal}
+              editing={editing}
+            />
+            <ArtistItem
+              label="Phone"
+              value={phoneVal}
+              setValue={setPhoneVal}
+              editing={editing}
+            />
+            <ArtistItem
+              label="Address"
+              value={addressVal}
+              setValue={setAddressObject}
+              editing={editing}
+              address={addressObject}
+            />
+            <ArtistItem
+              label="Website"
+              value={websiteVal}
+              setValue={setWebsiteVal}
+              editing={editing}
+            />
+            <Sheet>
+              <SheetTrigger className="mt-3 w-fit cursor-pointer text-sm text-blue-500 hover:underline">
+                Change Password
+              </SheetTrigger>
+              <SheetContent>
+                <div className="flex w-full flex-col gap-3">
+                  <div>
+                    <p className="text-2xl font-bold">Change Password</p>
+                    <p className="text-gray-500">
+                      {" "}
+                      Ensure your account is using a long, random password to
+                      stay secure.
+                    </p>
+                  </div>
+                  <div>
+                    <Label className="font-semibold">Current Password</Label>
+                    <PasswordInput />
+                  </div>
 
-                <div>
-                  <Label className="font-semibold">New Password</Label>
-                  <PasswordInput />
-                </div>
+                  <div>
+                    <Label className="font-semibold">New Password</Label>
+                    <PasswordInput />
+                  </div>
 
-                <div>
-                  <Label className="font-semibold">Confirm Password</Label>
-                  <PasswordInput />
-                </div>
+                  <div>
+                    <Label className="font-semibold">Confirm Password</Label>
+                    <PasswordInput />
+                  </div>
 
-                <Button disabled className="w-fit">
+                  <Button disabled className="w-fit">
+                    Save
+                  </Button>
+                </div>
+              </SheetContent>
+            </Sheet>
+            {editing && (
+              <div className="flex gap-3">
+                <Button
+                  className="w-fit"
+                  variant="secondary"
+                  onClick={() => cancelEdit()}
+                >
+                  Cancel
+                </Button>
+                <Button className="w-fit" onClick={() => onSave()}>
                   Save
                 </Button>
               </div>
-            </SheetContent>
-          </Sheet>
-          {editing && (
-            <div className="flex gap-3">
-              <Button
-                className="w-fit"
-                variant="secondary"
-                onClick={() => cancelEdit()}
-              >
-                Cancel
-              </Button>
-              <Button className="w-fit" onClick={() => onSave()}>
-                Save
-              </Button>
-            </div>
-          )}
+            )}
+          </div>
+          <div className="flex w-[50%] flex-col gap-3 rounded border border-gray-200 bg-white p-2.5">
+            <h1 className="text-xl font-semibold">Payment Information</h1>
+            <ArtistItem
+              label="Payment Type"
+              value={paymentType}
+              setValue={setPaymentType}
+              editing={editing}
+            />
+            {paymentType === "Check" && (
+              <ArtistItem
+                label="Payee Name"
+                value={payeeName}
+                setValue={setPayeeName}
+                editing={editing}
+              />
+            )}
+          </div>
         </div>
       )}
     </div>
