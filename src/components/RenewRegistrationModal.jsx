71f833b04d9cae81aff40c2bd2bbdb30d596ae34
George Nakashyan <gnakashyan@nasboces.org>
2025-03-27 15:56:51 -0400

    feat: add renew registration modal on page load

---
+++ src/components/RenewRegistrationModal.jsx
@@ -0,0 +1,43 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const RenewRegistrationModal = (openProp) => {
+  const [open, setOpen] = React.useState(openProp);
+  const navigate = useNavigate();
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Your Registration is About to Expire
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Your registration is about to expire. Please renew your registration
+            to ensure uninterrupted access to our services.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => setOpen(false)}>
+            Remind me Later
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={() => navigate("/registration-renewal")}>
+            Renew Registration
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default RenewRegistrationModal;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,20 +1,23 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import RenewRegistrationModal from "@/components/renewRegistrationModal";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/toaster";
 import { useIsMobile } from "@/hooks/use-mobile";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const MainLayout = () => {
   const isMobile = useIsMobile();
+  
 
   return (
-    <div className="max-w-screen flex min-h-screen flex-col bg-slate-50 overflow-x-hidden">
+    <div className="max-w-screen flex min-h-screen flex-col overflow-x-hidden bg-slate-50">
+      <RenewRegistrationModal />
       <SidebarProvider>
         {!isMobile && <AppSidebar variant="inset" collapsible="icon" />}
         <div className="flex flex-grow flex-col">
           {isMobile && <AppSidebar variant="inset" collapsible="icon" />}
           <div
-            className={`flex ${isMobile ? "max-w-[100vw]" : "max-w-[calc(100vw-var(--sidebar-width))]"}  grow items-start justify-start p-10 pt-14 overflow-hidden`}
+            className={`flex ${isMobile ? "max-w-[100vw]" : "max-w-[calc(100vw-var(--sidebar-width))]"}  grow items-start justify-start overflow-hidden p-10 pt-14`}
           >
             <Outlet />
           </div>
