0ecce95f7b1392f6c2d69760c0450e7c2f5d3734
George N <gnakashyan@nasboces.org>
2025-03-28 14:59:06 -0400

    feat: add file uploading and file type selection

--- src/pages/FileUploadPage.jsx
+++ src/pages/FileUploadPage.jsx
@@ -1,10 +1,37 @@
-import FileUploadForm from "@/components/FileUploadForm";
+import { DropZone } from "@/components/DropZone";
 import { Button } from "@/components/ui/button";
+import DataGrid from "@/components/ui/data-grid";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
 import Spinner from "@/components/ui/Spinner";
-import { useQueryForDataQuery } from "@/redux/api/quickbaseApi";
-import { signOut } from "@/redux/slices/authSlice";
+import { toast } from "@/components/ui/use-toast";
+import {
+  useAddOrUpdateRecordMutation,
+  useQueryForDataQuery,
+} from "@/redux/api/quickbaseApi";
+import { documentColumns } from "@/utils/TableColumns";
+import { downloadFile, getCurrentFiscalYearKey } from "@/utils/utils";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import { Loader2, UploadIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 import { handleSignout } from "@/utils/utils";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -19,9 +46,143 @@ const INSTRUCTIONS = [
 const FileUploadPage = () => {
   const [documentTypes, setDocumentTypes] = useState(null);
   const [fileInputState, setFileInputState] = useState(null);
+  const [selectedType, setSelectedType] = useState("");
+  const [fileUploads, setFileUploads] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [missingFiles, setMissingFiles] = useState([]);
+  const artist = localStorage.getItem("artist/org");
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const { data: artistsData, isLoading: isArtistDataLoading } =
+    useQueryForDataQuery(
+      user
+        ? {
+            from: import.meta.env.VITE_QUICKBASE_ARTISTS_TABLE_ID,
+            select: [46],
+            where: `{10.EX.${user.uid}}`,
+          }
+        : { skip: !user, refetchOnMountOrArgChange: true },
+    );
+
+  const { data: fileTypes, isLoading: isFileTypesLoading } =
+    useQueryForDataQuery({
+      from: import.meta.env.VITE_QUICKBASE_DOCUMENT_TYPES_TABLE_ID,
+      select: [3, 7, 31],
+    });
+  const { data: documentsData, isLoading: isDocumentsDataLoading } =
+    useQueryForDataQuery({
+      from: import.meta.env.VITE_QUICKBASE_ARTISTS_FILES_TABLE_ID,
+      select: [11, 9, 7, 12, 6, 14, 3, 10],
+      where: `{9.EX.${artist}}`,
+      sortBy: [{ fieldId: 10 }, { order: "DESC" }],
+    });
+
+  const [
+    addDocument,
+    {
+      isLoading: isAddDocumentLoading,
+      isSuccess: isAddDocumentSuccess,
+      isError: isAddDocumentError,
+    },
+  ] = useAddOrUpdateRecordMutation();
+
+  useEffect(() => {
+    if (isAddDocumentError) {
+      toast({
+        variant: "destructive",
+        title: "Error submitting documents",
+        description: addArtistDocumentRecordError.data.message,
+      });
+      setOpen(false);
+    } else if (isAddDocumentSuccess) {
+      toast({
+        variant: "success",
+        title: "Operation successful!",
+        description: "Your documents have been submitted.",
+      });
+      setOpen(false);
+      setSelectedType("");
+    }
+  }, [isAddDocumentError, isAddDocumentSuccess]);
+
+  const formatData = (docData) => {
+    const { data } = docData;
+    return data.map((record) => {
+      let versionNumber = [...record[7].value.versions];
+      versionNumber = versionNumber.pop().versionNumber;
+      return {
+        id: record[3].value,
+        fiscalYear: record[11].value,
+        documentType: record[6].value,
+        artist: record[9].value,
+        documentName: record[7].value.versions[0].fileName,
+        versionNumber: versionNumber,
+        file: record[7],
+      };
+    });
+  };
+
+  const uploadFile = async () => {
+    if (fileUploads === null) {
+      toast({
+        variant: "destructive",
+        title: "Error submitting documents",
+        description: "Please upload a file.",
+      });
+      return;
+    }
+    if (selectedType === "") {
+      toast({
+        variant: "destructive",
+        title: "Error submitting documents",
+        description: "Please select a file type",
+      });
+      return;
+    }
+    const fiscalYear = getCurrentFiscalYearKey();
+    const artist = localStorage.getItem("artist/org");
+    let base64 = await fileToBase64(fileUploads);
+    base64 = base64.split("base64,")[1];
+    addDocument({
+      to: import.meta.env.VITE_QUICKBASE_ARTISTS_FILES_TABLE_ID,
+      data: [
+        {
+          10: { value: fiscalYear },
+          9: { value: artist },
+          7: {
+            value: {
+              fileName: fileUploads.name,
+              data: base64,
+            },
+          },
+          6: { value: selectedType },
+        },
+      ],
+    });
+  };
+  const fileToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  };
+
+  const downloadTemplate = (file) => {
+    let versionNumber = [...file[7].value.versions];
+    versionNumber = versionNumber.pop().versionNumber;
+    downloadFile(
+      import.meta.env.VITE_QUICKBASE_DOCUMENT_TYPES_TABLE_ID,
+      7,
+      file[3].value,
+      versionNumber,
+    );
+  };
 
   const {
     data: documentTypesData,
@@ -66,6 +227,14 @@ const FileUploadPage = () => {
     setFileInputState(initialFileInputState);
   }
 
+  if (isDocumentsDataLoading || isFileTypesLoading) {
+    return (
+      <div className="flex h-full w-full justify-center pt-24">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col justify-center">
       <header className="pb-10">
@@ -83,15 +252,74 @@ const FileUploadPage = () => {
         </ul>
       </section>
       <section>
-        <FileUploadForm
-          documentTypes={documentTypes}
-          fileInputState={fileInputState}
-          setFileInputState={setFileInputState}
-        />
+        <div className="flex items-center gap-3">
+          <p className="text-sm text-gray-600">Upload a file: </p>
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+              <Button variant="outline" className="rounded-full">
+                <UploadIcon className="size-4" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-sm sm:max-w-lg">
+              <DialogHeader>
+                <DialogTitle className="mb-1">Upload a file</DialogTitle>
+              </DialogHeader>
+              <Separator />
+              <div className="flex flex-col gap-2">
+                <Label>File Type</Label>
+                <Select
+                  value={selectedType}
+                  onValueChange={(e) => setSelectedType(e)}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a file type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {fileTypes.data.map(
+                      (f) =>
+                        (f[31].value == "Vendor Application" ||
+                          f[31].value == "W-9") && (
+                          <SelectItem value={f[31].value} key={f[31].value}>
+                            {f[31].value}
+                          </SelectItem>
+                        ),
+                    )}
+                  </SelectContent>
+                </Select>
+                <DropZone setUploadedFile={setFileUploads} />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline">Close</Button>
+                  </DialogClose>
+                  <Button
+                    type="submit"
+                    onClick={() => uploadFile()}
+                    disabled={isAddDocumentLoading}
+                  >
+                    {isAddDocumentLoading && (
+                      <Loader2 className="animate-spin" />
+                    )}
+                    Submit
+                  </Button>
+                </DialogFooter>
+              </div>
+            </DialogContent>
+          </Dialog>
+        </div>
       </section>
-      <Button variant="outline" onClick={() => handleSignout(dispatch, navigate)}>
+      <Button
+        variant="outline"
+        onClick={() => handleSignout(dispatch, navigate)}
+      >
         Sign Out
       </Button>
+      {documentsData && (
+        <DataGrid
+          data={formatData(documentsData)}
+          columns={documentColumns}
+          readOnly
+        />
+      )}
     </div>
   );
 };
