1da7cd5f8c40f0603f467e6fe1682a20b0a38035
George Nakashyan <gnakashyan@nasboces.org>
2025-04-09 09:03:15 -0400

    feat: only show current documents on artist documents page

--- src/pages/ArtistDocumentsPage.jsx
+++ src/pages/ArtistDocumentsPage.jsx
@@ -69,8 +69,8 @@ const ArtistDocumentsPage = () => {
   const { data: documentsData, isLoading: isDocumentsDataLoading } =
     useQueryForDataQuery({
       from: import.meta.env.VITE_QUICKBASE_ARTISTS_FILES_TABLE_ID,
-      select: [11, 9, 7, 12, 6, 14, 3, 10],
-      where: `{9.EX.${artist}}`,
+      select: [3, 6, 7, 9, 10, 11, 12, 14, 17],
+      where: `{9.EX.${artist}} AND {17.EX.${true}}`,
       sortBy: [{ fieldId: 10 }, { order: "DESC" }],
     });
 
